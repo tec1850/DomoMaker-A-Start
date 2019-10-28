5ef112978fa68956f6f4cbf43ad849fc797b742e
Taylor Cox <taylorecox@yahoo.com>
2019-10-28 05:41:21 -0400

    Final fix???

--- server/app.js
+++ server/app.js
@@ -13,10 +13,10 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGOLAB_URI || 'mongodb://localhost/DomoMaker';
 
 mongoose.connect(dbURL, (err) => {
-  if (err) {
-    console.log('Could not connect to database');
-    throw err;
-  }
+    if (err) {
+        console.log('Could not connect to database');
+        throw err;
+    }
 });
 
 // pull in our routes
@@ -24,21 +24,21 @@ const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-  extended: true,
+    extended: true,
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(cookieParser());
 
 router(app);
 
 app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
+    if (err) {
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
 });
